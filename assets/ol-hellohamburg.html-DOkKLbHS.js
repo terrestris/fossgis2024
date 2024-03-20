f3c458c85c9374025ff34bee89cd775cda83d4f2
marcjansen <jansen@terrestris.de>
2024-03-20 16:21:41 +0000

    Deploying to gh-pages from @ terrestris/fossgis2024@5df6cfe886d5e062cfe2b58c80999c8e29147da2 🚀

--- assets/ol-hellohamburg.html-DOkKLbHS.js
+++
@@ -1,1 +0,0 @@
-import{m as t,o}from"./index-D3v3MZ7_.js";/* empty css              */const a="https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json";t("map",a).then(e=>e.getView().animate({center:o([10.01,53.55]),zoom:9.5}));
---
+++ assets/ol-hellohamburg.html-frnuEKWd.js
@@ -0,0 +1,1 @@
+import{a0 as a,a2 as t}from"./index-DBbjyXw1.js";/* empty css              */const o="https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json";a("map",o).then(e=>e.getView().animate({center:t([10.01,53.55]),zoom:9.5}));
