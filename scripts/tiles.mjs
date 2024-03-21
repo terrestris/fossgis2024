0f2cdea0abf945e6ad888256782bf5b4fdae9350
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-03-21 11:34:45 +0100

    More OpenLayers slides and examples

---
+++ scripts/tiles.mjs
@@ -0,0 +1,17 @@
+import { XYZ } from 'ol/source.js';
+import { fromLonLat as ll } from 'ol/proj.js';
+
+const source = new XYZ({
+	url: 'https://server/path/to/tiles/{z}/{x}/{y}.png',
+});
+const extent = [...ll([15.5, 47.5]), ...ll([16.0, 48.0])];
+console.log(extent)
+const tileGrid = source.getTileGrid();
+const getUrl = source.getTileUrlFunction();
+const offlineTiles = [];
+for (let z = 0; z <= 15; ++z) {
+	tileGrid.forEachTileCoord(extent, z, (tileCoord) => {
+		offlineTiles.push(getUrl(tileCoord));
+	});
+};
+console.log(offlineTiles);
